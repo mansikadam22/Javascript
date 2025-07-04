bdb83fc1bb38e4977cf50a9cf9f64811c63dc23b
Mansi Kadam <kadammansi455@gmail.com>
2025-07-04 14:01:37 +0530

    commit

---
+++ Operators.js
@@ -0,0 +1,103 @@
+ //Arithmatic Operators
+let a = 10;
+let b = 3;
+console.log('Arithmatic Operators')
+console.log("Addition:",a+b)
+console.log("Substraction:",a-b)
+console.log("Multiplication:",a*b)
+console.log("Division:",a/b)
+console.log("Modulus (Remainder):",a%b)
+console.log("Increment:",a++)
+console.log("Decrement:",b--)
+console.log("Exponentiation:",a**b)
+
+
+//Assignment operators
+let c = 15;
+console.log('Assignment operators')
+console.log("Assignment:",c=20)
+console.log("Addition Assignment:",c += 30)
+console.log("Subtraction assignment:",c-=20)
+console.log("Multiplication assignment:",c*=20)//
+console.log("Division assignment:",c/=20)//
+console.log("Modulus assignment:",c%=20)//
+console.log("Exponentiation assignment:",c**=20)//
+
+
+//Comparison operators
+let d = 10;
+let e = 20;
+let age = 20;
+let result = (age>=18? "Adult":"minor")
+console.log('Comparison operators')
+console.log("Equal to:",d==e)
+console.log("Strict equal to:",d===e)
+console.log("Not equal value:",d!=e)
+console.log("Strict Not equal value:",d!==e)
+console.log("Greater than:",d>e)
+console.log("Less than:",d<e)
+console.log("Greater than equal to:",d>=e)
+console.log("Less than equal to:",d<=e)
+console.log("Result",result);
+
+
+//Logical operators
+let f = 34;
+let g = 77;
+console.log('Logical operators')
+console.log("Logical AND:",f<g && f!=g)
+console.log("Logical OR:",f>g || f<g)
+console.log("Logical NOT:",!f)
+
+//Bitwise operators
+let h = 88;
+console.log('Bitwise operators')
+console.log("Bitwise AND:",h&2)
+console.log("Bitwise OR:",h|2)
+console.log("Bitwise XOR:",h^2)
+console.log("Bitwise NOT:",~h)
+console.log("Left Shift:",h<<2)
+console.log("Right Shift:",h>>2)
+console.log("Zero-fill Right Shift:",h>>>2)
+
+
+//Type Operators
+var no = 22;
+let name = "mansi";
+console.log('Type Operators')
+console.log("type of no:",typeof no)
+console.log("type of name:",typeof name)
+
+
+//String Operators
+var string1='Mansi'
+var string2='Kadam'
+console.log('String Oprators')
+console.log('String 1:',string1)
+console.log('String 2:',string2)
+console.log('Concatnation:',string1+string2)
+console.log('Concatenation assignment',string1+=string2)
+
+
+//Ternary Operator (Conditional Operator)
+var i=10
+console.log('Ternary Oprators')
+console.log('Value of I:',i)
+console.log('I is 10 :',i==10?'True':'False')
+console.log('I is 60 :',i==60?'True':'False')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ variables.js
@@ -0,0 +1,6 @@
+let name = 'Mansi';
+var Roll_no = 12;
+const age = 17;
+console.log("Name:",name);
+console.log("Rollno:",Roll_no);
+console.log("Age:",age);
